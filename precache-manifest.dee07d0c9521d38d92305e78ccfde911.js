e057ad17ec7045ed0f20287c9b4be11a2405a37f
Ashish Gupta <ashish.gupta@externals.hilti.com>
2020-04-29 00:31:46 +0530

    Updates

---
+++ precache-manifest.dee07d0c9521d38d92305e78ccfde911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d045a570ff524163b07f59d520b9fe69",
+    "url": "/scroll-header/index.html"
+  },
+  {
+    "revision": "d734d3a1fc9323a58435",
+    "url": "/scroll-header/static/css/main.bf64bbf7.chunk.css"
+  },
+  {
+    "revision": "a50824bb70ba1c8c9b1d",
+    "url": "/scroll-header/static/js/2.d773461b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scroll-header/static/js/2.d773461b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d734d3a1fc9323a58435",
+    "url": "/scroll-header/static/js/main.a56c49e7.chunk.js"
+  },
+  {
+    "revision": "cd5e99f53990f5029b06",
+    "url": "/scroll-header/static/js/runtime-main.05db94c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scroll-header/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf9ade3546368eef2e7d15ee752708db.js"
+  "/scroll-header/precache-manifest.dee07d0c9521d38d92305e78ccfde911.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scroll-header/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
